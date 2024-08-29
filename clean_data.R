d5d7cee2c523b6b53d2bd2c1a794d55cfdfd7f28
Yiiining <yining2230@gmail.com>
2024-08-29 16:36:57 +0800

    turn start_t into numeric

--- clean_data.R
+++ clean_data.R
@@ -4,7 +4,19 @@ library(dplyr)
 library(lubridate)
 col_na_count <- colSums(is.na(dummy_data))
 print(col_na_count)
+library(tidyr)
+dummy_data <- drop_na(dummy_data, start_t)
 
 library(stringr)
-dummy_data$start_t <- str_replace_all(dummy_data$start_t, "/[1-9]|/1[0-2]", "")
+dummy_data$start_t <- as.numeric(as.character(
+  str_replace_all(dummy_data$start_t, "/[1-9]|/1[0-2]", "")
+  ))
+
+dummy_data <- dummy_data %>%
+  filter(start_t < 2024) %>%
+  mutate(start_t = case_when(
+    start_t < 1911 ~ start_t + 1911,
+    TRUE ~ start_t
+  ))
+
 print(dummy_data)
