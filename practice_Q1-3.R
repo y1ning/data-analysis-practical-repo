35db7c3463789d62ceb66b8b2941573fc1bb0ec2
Yiiining <yining2230@gmail.com>
2024-08-30 14:05:36 +0800

    利用format把只有年份的start_t變成%Y-%m-%e(01-01)

--- practice_Q1-3.R
+++ practice_Q1-3.R
@@ -53,6 +53,5 @@ print(paste("number of hypertension =", n_hypertension, sep=" "))
 #Q5
 data$start_t <- as.Date(data$start_t, format = "%Y")
 data <- data %>%
-  mutate(start_t = as.Date(paste0(start_t, "-01-01")))
-data$consult_date <- as.Date(data$consult_date, format = "%Y"-"%m"-"%e")
+  mutate(start_t = as.Date(paste0(format(start_t, "%Y"), "-01-01"), format = "%Y-%m-%e"))
 
